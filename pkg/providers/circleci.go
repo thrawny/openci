40443b1c0364c1e12bcd1fafb4724972b0bf92be
Jonas Lergell <jle@speedledger.se>
2019-08-22 15:17:12 +0200

    Use more specific url for circleci

--- pkg/providers/circleci.go
+++ pkg/providers/circleci.go
@@ -9,10 +9,10 @@ const circleBaseURL = "https://circleci.com"
 
 type CirleCI struct{}
 
-func (CirleCI) GetProjectURL(remote git.Remote) string {
+func (CirleCI) GetProjectURL(remote git.Remote, branch string) string {
 	gitProvider := "gh"
 	if remote.Domain == "bitbucket.org" {
 		gitProvider = "bb"
 	}
-	return fmt.Sprintf("%s/%s/%s/%s", circleBaseURL, gitProvider, remote.Org, remote.Project)
+	return fmt.Sprintf("%s/%s/%s/workflows/%s/tree/%s", circleBaseURL, gitProvider, remote.Org, remote.Project, branch)
 }
--- pkg/providers/wercker.go
+++ pkg/providers/wercker.go
@@ -9,6 +9,6 @@ const werckerBaseURL = "https://app.wercker.com"
 
 type Wercker struct{}
 
-func (Wercker) GetProjectURL(remote git.Remote) string {
+func (Wercker) GetProjectURL(remote git.Remote, branch string) string {
 	return fmt.Sprintf("%s/%s/%s", werckerBaseURL, remote.Org, remote.Project)
 }
